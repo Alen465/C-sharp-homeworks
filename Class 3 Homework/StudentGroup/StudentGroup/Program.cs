5eb4cadd6a00b08eb440a48bf37e534767617b3d
Alen465 <alen.medic2006@gmail.com>
2026-03-29 21:56:50 +0200

    Add files via upload

---
+++ Class 3 Homework/StudentGroup/StudentGroup/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class StudentGroup
+{
+    static void Main()
+    {
+        string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+        string[] studentsG2 = { "Ana", "Bojan", "Mila", "Ivan", "Petar" };
+
+        Console.Write("Enter student group (1 or 2): ");
+        int group = Convert.ToInt32(Console.ReadLine());
+
+        if (group == 1)
+        {
+            Console.WriteLine("The Students in G1 are:");
+            foreach (string student in studentsG1)
+            {
+                Console.WriteLine(student);
+            }
+        }
+        else if (group == 2)
+        {
+            Console.WriteLine("The Students in G2 are:");
+            foreach (string student in studentsG2)
+            {
+                Console.WriteLine(student);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid group number!");
+        }
+    }
+}
---
+++ Class 3 Homework/SumOfEven/SumOfEven/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class SumOfEven
+{
+    static void Main()
+    {
+        int[] numbers = new int[6];
+        int sumEven = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"Enter integer no.{i + 1}: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            if (numbers[i] % 2 == 0) // check if even
+            {
+                sumEven += numbers[i];
+            }
+        }
+
+        Console.WriteLine("The result is: " + sumEven);
+    }
+}
