3606880f8799c1fc296e080fb76f3124e9dd80cb
Alen465 <alen.medic2006@gmail.com>
2026-03-29 21:57:20 +0200

    Add files via upload

---
+++ Class 4 Homework/AgeCalculator/AgeCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class AgeCalculatorApp
+{
+    static void Main()
+    {
+        Console.Write("Enter your birthday (yyyy-mm-dd): ");
+        DateTime birthday = Convert.ToDateTime(Console.ReadLine());
+
+        int age = AgeCalculator(birthday);
+
+        Console.WriteLine($"Your age is: {age}");
+    }
+
+    static int AgeCalculator(DateTime birthday)
+    {
+        DateTime today = DateTime.Today;
+
+        int age = today.Year - birthday.Year;
+
+        // If birthday hasn't happened yet this year, subtract 1
+        if (birthday.Date > today.AddYears(-age))
+        {
+            age--;
+        }
+
+        return age;
+    }
+}
