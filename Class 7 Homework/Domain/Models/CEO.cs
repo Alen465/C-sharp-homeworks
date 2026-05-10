9809e754eefa4e1d0417a1c0579d79a2616bfea2
Alen465 <alen.medic2006@gmail.com>
2026-05-11 01:42:27 +0200

    Add files via upload

---
+++ Class 7 Homework/Domain/Models/CEO.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class CEO : Employee
+    {
+        public Employee[] Employees { get; set; }
+        public int Shares { get; set; }
+
+        public double SharesPrice { get; private set; }
+
+        public CEO(
+            int id,
+            string firstName,
+            string lastName,
+            int age,
+            double salary,
+            Seniority seniority,
+            Employee[] employees,
+            int shares)
+            : base(id, firstName, lastName, age, salary, seniority)
+        {
+            Employees = employees;
+            Shares = shares;
+        }
+
+        public void UpdateSharesPrice(double amount)
+        {
+            SharesPrice = amount;
+        }
+
+        public void PrintEmployees()
+        {
+            foreach (Employee e in Employees)
+            {
+                Console.WriteLine($"{e.FirstName} {e.LastName}");
+            }
+        }
+
+        public override double CalculateAnnualBonus()
+        {
+            return Salary + (Shares * SharesPrice);
+        }
+    }
+}
