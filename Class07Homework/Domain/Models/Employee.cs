ea2433d4a9cd21e5b06ba0d266530c49db9c80e3
Alen465 <alen.medic2006@gmail.com>
2026-05-11 01:39:45 +0200

    Add files via upload

---
+++ Class07Homework/Domain/Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        protected double Salary { get; set; }
+        public Seniority Seniority { get; set; }
+
+        public Employee(
+            int id,
+            string firstName,
+            string lastName,
+            int age,
+            double salary,
+            Seniority seniority)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+            Seniority = seniority;
+        }
+
+        public virtual string GetDetails()
+        {
+            return $"[{Id}] {FirstName} {LastName}, Age: {Age}, Salary: {Salary:C}, Seniority: {Seniority}";
+        }
+
+        public virtual double CalculateAnnualBonus()
+        {
+            return Salary * 0.1;
+        }
+    }
+}
---
+++ Class07Homework/Homework/Program.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Enums;
+using Domain.Models;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Developer dev1 = new Developer(
+                1, "Bob", "Bobsky", 28, 3000,
+                Seniority.Junior, "C#");
+
+            Developer dev2 = new Developer(
+                2, "John", "Johnsky", 30, 3500,
+                Seniority.Senior, "Java");
+
+            Manager manager1 = new Manager(
+                3, "Sarah", "Smith", 40, 5000,
+                Seniority.Lead, "IT");
+
+            Manager manager2 = new Manager(
+                4, "Mike", "Johnson", 38, 4800,
+                Seniority.Senior, "HR");
+
+            Contractor contractor = new Contractor(
+                5, "David", "Williams", 29, 2000,
+                Seniority.Junior, 160, 25, manager1);
+
+            Employee[] employees =
+            {
+                dev1, dev2, manager1, manager2, contractor
+            };
+
+            CEO ceo = new CEO(
+                6, "Alice", "Alison", 35, 8000,
+                Seniority.Lead, employees, 500);
+
+            ceo.UpdateSharesPrice(41);
+
+            Console.WriteLine(ceo.GetDetails());
+
+            Console.WriteLine();
+            Console.WriteLine("Employees:");
+
+            ceo.PrintEmployees();
+
+            Console.WriteLine();
+
+            Console.WriteLine("CEO Annual Bonus: " + ceo.CalculateAnnualBonus());
+
+            Console.ReadLine();
+        }
+    }
+}
